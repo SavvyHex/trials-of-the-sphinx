84169da8272595c246299bda5f71d9b9b30bc03e
SavvyHex <paisaketh@gmail.com>
2022-12-08 20:24:56 +0530

    modified:   tests/terminal streak quiz/question.py

--- tests/terminal streak quiz/question.py
+++ tests/terminal streak quiz/question.py
@@ -1,3 +1,19 @@
+import random
+
 class Question:
     def __init__(self) -> None:
-        pass
+        self.op = random.choice(["+", "-", "*", "/"])
+        self.a = random.randint(1, 20)
+        self.b = random.randint(1, 20)
+        self.get_ans()
+
+    def get_ans(self) -> None:
+        op = self.op
+        if op == "+":
+            self.ans = self.a + self.b
+        elif op == "-":
+            self.ans = self.a - self.b
+        elif op == "*":
+            self.ans = self.a * self.b
+        elif op == "/":
+            self.ans = self.a / self.b
