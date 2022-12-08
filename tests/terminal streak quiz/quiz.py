4713c222491bc6e8bd64ff378ce4b943b131a6bf
SavvyHex <paisaketh@gmail.com>
2022-12-08 20:33:27 +0530

    Completed simple math terminal quiz

--- tests/terminal streak quiz/quiz.py
+++ tests/terminal streak quiz/quiz.py
@@ -5,16 +5,24 @@ from question import Question
 class Game:
     def __init__(self) -> None:
         self.score = 0
-        self.operations = ["+", "-", "*", "/"]
+        self.get_question()
     
-    def get_question(self) -> Question:
-        pass
+    def get_question(self) -> None:
+        self.question = Question()
 
     def run(self) -> None:
-        while True:
-            pass
+        run = True
+        while run:
+            print(self.question)
+            if float(input()) == self.question.ans:
+                print("Correct")
+                self.score += 1
+                self.get_question()
+            else:
+                print(f"WRONG!!! the correct answer is {self.question.ans}")
+                print(f"Your score is {self.score}")
+                run = False
 
-if __name__ == "":
+if __name__ == "__main__":
     game = Game()
     game.run()
-    input()
