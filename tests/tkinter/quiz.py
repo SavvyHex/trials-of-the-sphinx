9e786be605280da7a3640462c6913bfb5be6ebd5
SavvyHex <paisaketh@gmail.com>
2022-12-29 15:34:02 +0530

    finally done

--- tests/tkinter/quiz.py
+++ tests/tkinter/quiz.py
@@ -19,7 +19,7 @@ class QuizApp:
                                                      width=680,
                                                      fill="black",
                                                      font=(
-                                                         'Ariel', 15, 'italic')
+                                                         'Helvetica', 15, 'italic')
                                                      )
         self.canvas.grid(row=2, column=0, columnspan=2, pady=50)
         self.display_question()
@@ -29,7 +29,7 @@ class QuizApp:
         self.opts = self.radio_buttons()
         self.display_options()
 
-        self.feedback = Label(self.window, pady=10, font=("ariel", 15, "bold"))
+        self.feedback = Label(self.window, pady=10, font=("Helvetica", 15, "bold"))
         self.feedback.place(x=300, y=380)
 
         self.buttons()
@@ -37,8 +37,7 @@ class QuizApp:
         self.window.mainloop()
     
     def display_title(self):
-        title = Label(self.window, text="Trials of the Sphinx",
-                        width=50, bg="green", fg="white", font=("ariel", 20, "bold"))
+        title = Label(self.window, text="Trials of the Sphinx", width=63, bg="green", fg="white", font=("Helvetica", 20, "bold"))
         title.place(x=0, y=2)
 
     def display_question(self):
@@ -49,7 +48,7 @@ class QuizApp:
         choice_list = list()
         y = 220
         for option in self.question.options:
-            radio_btn = Radiobutton(self.window, text="", variable=self.user_answer, value='', font=("ariel", 14))
+            radio_btn = Radiobutton(self.window, text="", variable=self.user_answer, value='', font=("Helvetica", 14))
             choice_list.append(radio_btn)
             radio_btn.place(x=200, y=y)
             y += 40
@@ -77,10 +76,10 @@ class QuizApp:
 
     def buttons(self):
         next_button = Button(self.window, text="Next", command=self.next_btn,
-                            width=10, bg="green", fg="white", font=("ariel", 16, "bold"))
+                            width=10, bg="green", fg="white", font=("Helvetica", 16, "bold"))
         next_button.place(x=350, y=460)
         quit_button = Button(self.window, text="Quit", command=self.window.destroy,
-                            width=5, bg="red", fg="white", font=("ariel", 16, " bold"))
+                            width=5, bg="red", fg="white", font=("Helvetica", 16, " bold"))
         quit_button.place(x=700, y=50)
 
     def display_result(self):
