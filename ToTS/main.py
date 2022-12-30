98f34a993c50f449052c6e87b6d9fc88bad42640
SavvyHex <paisaketh@gmail.com>
2022-12-30 15:12:57 +0530

    added name

--- ToTS/main.py
+++ ToTS/main.py
@@ -6,6 +6,11 @@ class QuizApp():
         self.window = tkinter.Tk()
         self.window.title("Trials of the Sphinx")
         self.window.geometry("850x530")
+
+        self.canvas = tkinter.Canvas(width=800, height=250)
+        self.name = self.canvas.create_text(400, 125, text="Trials of the Sphinx", width=680, fill="black", font=('Helvetica', 20, 'bold'))
+        self.canvas.grid(row=2, column=0, columnspan=2, pady=30)
+
         self.window.mainloop()
 
 if __name__ == "__main__":
