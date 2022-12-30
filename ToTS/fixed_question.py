d2067241c92aabdadbb15901545db163a7f6dc40
SavvyHex <paisaketh@gmail.com>
2022-12-30 17:13:18 +0530

    typing

--- ToTS/fixed_question.py
+++ ToTS/fixed_question.py
@@ -1,5 +1,5 @@
 class Question():
-    def __init__(self, question:str, options:list, ans:str) -> None:
+    def __init__(self, question:str, options:list[str, str, str, str], ans:str) -> None:
         self.question = question
         self.options = options
         self.ans = ans
