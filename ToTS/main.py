d9cae94a9ac58c04357f2445b23464728845a6fd
SavvyHex <paisaketh@gmail.com>
2022-12-30 23:04:19 +0530

    added bg for the index file

--- ToTS/main.py
+++ ToTS/main.py
@@ -6,9 +6,14 @@ class QuizApp():
     def __init__(self) -> None:
         self.window = tkinter.Tk()
         self.window.title("Trials of the Sphinx")
-        self.window.geometry("850x530")
+        self.window.geometry("620x450")
 
         self.canvas = tkinter.Canvas(width=800, height=250)
+
+        ph = PhotoImage(file="ToTS\main_bg.png")
+        bgimg = Label(self.window, image=ph)
+        bgimg.place(x=0, y=0)
+        
         self.name = self.canvas.create_text(400, 70, text="Trials of the Sphinx", width=680, fill="black", font=('Helvetica', 20, 'bold'))
         self.canvas.grid(row=2, column=0, columnspan=2, pady=30)
         self.buttons()
@@ -17,9 +22,9 @@ class QuizApp():
 
     def buttons(self) -> None:
         streakbutton = tkinter.Button(self.window, text="Math Streak Quiz", command=self.start_streak, width=15, bg="blue", fg="white", font=("Helvetica", 16, " bold"))
-        streakbutton.place(x=320, y=200)
+        streakbutton.place(x=200, y=180)
         strucbutton = tkinter.Button(self.window, text="Structured Quiz", command=self.structured_quiz, width=15, bg="green", fg="white", font=("Helvetica", 16, " bold"))
-        strucbutton.place(x=320, y=240)
+        strucbutton.place(x=200, y=240)
 
     def start_streak(self) -> None:
         self.window.destroy()
