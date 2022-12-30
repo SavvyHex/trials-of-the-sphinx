26d91b479a1ee13dbbc273e7d00af78475a3ad88
SavvyHex <paisaketh@gmail.com>
2022-12-30 20:03:46 +0530

    structured quiz form done

--- ToTS/forms.py
+++ ToTS/forms.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from structured_quiz import StructuredQuiz
 
 class RegForm():
     def __init__(self) -> None:
@@ -24,7 +25,24 @@ class StrucQuizForm():
     def __init__(self) -> None:
         self.window = Tk()
         self.window.title("Quiz Configuration")
-        self.window.geometry("300x300")
+        self.window.geometry("300x150")
+
+        a = Label(self.window, text="Subject").grid(row=0, column=0)
+        b = Label(self.window, text="Number of Questions").grid(row=1, column=0)
+
+        self.sub = StringVar()
+        self.n_q = StringVar()
+        self.sub.set("General Knowledge")
+        self.n_q.set("5")
+        self.pick_sub = OptionMenu(self.window, self.sub, *["Science", "General Knowledge"]).grid(row=0, column=1)
+        self.num_questions = OptionMenu(self.window, self.n_q, *list(map(str, list(range(5, 51, 5))))).grid(row=1, column=1)
+        
+        submit_button = Button(self.window, text="Submit", command=self.submit).grid(row=3, column=0)
+        self.window.mainloop()
+
+    def submit(self):
+        self.window.destroy()
+        StructuredQuiz(self.n_q, self.sub)
 
 if __name__ == "__main__":
     StrucQuizForm()
